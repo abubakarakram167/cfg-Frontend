7d2091516c50d9eb7942a6e5689860e6a72f57e5
abrar farhad <abrar.farhad.af@gmail.com>
2021-04-13 23:34:59 +0600

    migrating to new pc

---
+++ src/pages/user-home-page/index.jsx
@@ -0,0 +1,134 @@
+import React, {useState} from 'react';
+import AppSideBar from './AppSidebar';
+import CreatePost from './create-post-box';
+import './style.css';
+import UserHomeHeader from './admin-header';
+import PostDetails from './post-details';
+
+export default function UserHomePage() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const [fakeData, setFakeData] = useState([
+    {
+      id: 1,
+      caption: 'hello sir jermaine',
+      media:
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+      comments: [
+        {
+          id: 1,
+          commentText: 'this is a comment',
+          commentReplies: ['hello', 'world'],
+        },
+      ],
+    },
+    {
+      id: 2,
+      caption: 'hello sir jermaine',
+      media:
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+      comments: [
+        {
+          id: 1,
+          commentText: 'this is a comment',
+          commentReplies: ['hello', 'world'],
+        },
+        {
+          id: 2,
+          commentText: 'this is another',
+          commentReplies: ['hello', 'world'],
+        },
+      ],
+    },
+    {
+      id: 3,
+      caption: 'hello sir jermaine',
+      media: '',
+      comments: [
+        {
+          id: 1,
+          commentText: 'this is a comment',
+          commentReplies: ['hello', 'world'],
+        },
+      ],
+    },
+    {
+      id: 4,
+      caption: 'This is a post without a comment',
+      media:
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+      comments: [],
+    },
+  ]);
+
+  const createComment = (id, commentText) => {
+    return {
+      id,
+      commentText,
+      commentReplies: [],
+    };
+  };
+
+  const toggleDrawerOpen = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+  return (
+    <div>
+      {/* <button onClick={toggleDrawerOpen}>Toggle</button>
+            hello world
+            <AppSideBar drawerOpen={drawerOpen} /> */}
+      <UserHomeHeader />
+
+      <div className='user-home-page-content'>
+        <div className='user-home-left'>Left</div>
+        <div className='user-home-center'>
+          <CreatePost />
+          {fakeData.map((element, index) => {
+            const addComment = (comment, postId) => {
+              setFakeData(
+                fakeData.map((data) => {
+                  if (postId === data.id) {
+                    data.comments.push(
+                      createComment(data.comments.length + 1, comment),
+                    );
+                  }
+                  return data;
+                }),
+              );
+            };
+
+            const addReplyAction = (postId, commentId, replyText) => {
+              console.log('add reply executed');
+              console.log(postId);
+              console.log(commentId);
+              setFakeData(
+                fakeData.map((data) => {
+                  if (postId === data.id) {
+                    data.comments.map((comment) => {
+                      if (comment.id === commentId) {
+                        comment.commentReplies.push(replyText);
+                      }
+                      return comment;
+                    });
+                  }
+                  console.log(data);
+                  return data;
+                }),
+              );
+            };
+
+            return (
+              <PostDetails
+                key={index}
+                post={element}
+                addCommentData={addComment}
+                addReplyAction={addReplyAction}
+              />
+            );
+          })}
+        </div>
+        <div className='user-home-right'>Right</div>
+      </div>
+    </div>
+  );
+}
