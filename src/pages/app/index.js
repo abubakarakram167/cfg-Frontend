2d64b440b872b094691695613238d4375f11c28a
abrar farhad <abrar.farhad.af@gmail.com>
2021-06-14 03:12:11 +0600

    fix

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import configureStore from '../../redux/store/index';
-import { socket } from 'socket'
+import {socket} from 'socket';
 import RouteComponent from './route-component';
-export default function index() {
+export default function MainApp() {
   console.log(configureStore());
   const login = false;
   useEffect(() => {
