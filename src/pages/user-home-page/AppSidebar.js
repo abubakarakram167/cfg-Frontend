232a296b57355e52dacf3c850d6068e818e07742
abubakar <abubakarakram167@gmail.com>
2021-10-02 21:35:02 +0500

    make remove s

--- src/pages/user-home-page/AppSidebar.js
+++ src/pages/user-home-page/AppSidebar.js
@@ -265,7 +265,7 @@ const AppSidebar = (props) => {
                   justifyContent: 'space-between',
                   alignItems: 'center',
                 }}>
-                <div>My Conversationss</div>
+                <div>My Conversations</div>
                 {conversationExtended ? (
                   <ExpandLess
                     onClick={toggleExpansion}
