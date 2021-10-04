867eed8aed049f20810895e4878dd26dce9f3123
abubakar <abubakarakram167@gmail.com>
2021-10-04 09:58:50 +0500

    fixes journal issues and add forms items in journal modal

--- src/components/JournalModal.js
+++ src/components/JournalModal.js
@@ -7,12 +7,16 @@ import Media from 'redux/services/media';
 import {baseUrl} from 'utils/axios';
 import {useDispatch, useSelector} from 'react-redux';
 import moment from 'moment';
+import {Select, MenuItem} from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
 import {
   createJournal,
   getSpecificJournal,
   updateJournal,
 } from '../redux/actions/journal';
+import './journalModal.css';
 import _ from 'lodash';
+import {KeyboardDatePicker} from '@material-ui/pickers';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -25,6 +29,19 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
     justifyContent: 'space-around',
     flexDirection: 'column',
+    overflowY: 'scroll',
+  },
+  checkboxRoot: {
+    marginTop: -8,
+    marginLeft: -8,
+  },
+  datePicker: {
+    '& .MuiFormLabel-root': {
+      paddingLeft: 10,
+    },
+    '& .MuiInputBase-root': {
+      paddingLeft: 8,
+    },
   },
 }));
 
@@ -33,7 +50,9 @@ export default function (props) {
   const [modalStyle] = React.useState(getModalStyle);
   const [innerContent, setInnerContent] = useState('');
   const [isContentChange, setContentChanged] = useState(false);
-  const [journalData, setjournalData] = useState({});
+  const [journalData, setjournalData] = useState({
+    type: 'goal',
+  });
   const dispatch = useDispatch();
   const user = JSON.parse(localStorage.getItem('current-user'));
   const journalId = props.journalId;
@@ -73,13 +92,16 @@ export default function (props) {
     const payload = {
       user_id: user.id,
       subject: !_.isEmpty(journalData) ? journalData.subject : props.subject,
-      start_date: moment().format('YYYY-MM-DD'),
-      end_date: moment().format('YYYY-MM-DD'),
+      start_date: moment(journalData.start_date).format('YYYY-MM-DD'),
+      end_date: moment(journalData.end_date).format('YYYY-MM-DD'),
       detail: innerContent,
       log_date: moment().format('YYYY-MM-DD'),
       status: 'nothing',
-      type: 'goal',
+      type: journalData.type,
       parent: 'sample',
+      track_my_goal: !_.isEmpty(journalData)
+        ? journalData.track_my_goal
+        : props.track_my_goal,
     };
 
     if (!journalId) {
@@ -120,12 +142,13 @@ export default function (props) {
     <Modal
       open={props.show}
       aria-labelledby='simple-modal-title'
-      aria-describedby='simple-modal-description'>
+      aria-describedby='simple-modal-description'
+      style={{backgroundColor: 'rgb(8 8 8 / 90%)'}}>
       <div style={modalStyle} className={classes.paper}>
-        <h2 style={{textAlign: 'center', color: '#5f5b5b', marginBottom: 40}}>
+        <h2 className='subject-heading'>
           {!_.isEmpty(journalData) ? journalData.subject : props.subject}
         </h2>
-        <div>
+        <div id='internal-editor'>
           <SunEditor
             onClickSmartClick={(id) => {}}
             onContentSave={(content) => setInnerContent(content)}
@@ -134,26 +157,123 @@ export default function (props) {
             onImageUploadBefore={handleImageUploadBefore}
             changeHeight={true}
             showToolbar={true}
+            modalType='internal'
           />
         </div>
+        <div className='journal-modal-subitems'>
+          <span style={{marginRight: 50}}>Type:</span>
+          <Select
+            labelId='demo-simple-select-filled-label'
+            id='demo-simple-select-filled'
+            onChange={(e) =>
+              setjournalData({
+                ...journalData,
+                type: e.target.value,
+              })
+            }
+            variant='filled'
+            value={!_.isEmpty(journalData) && journalData.type}
+            label='Type'
+            required>
+            <MenuItem value={'goal'}>Goal</MenuItem>
+            <MenuItem value={'journey'}>Journey</MenuItem>
+            <MenuItem value={'journal'}>Journal</MenuItem>
+          </Select>
+        </div>
+        {!_.isEmpty(journalData) && journalData.type === 'goal' && (
+          <div className='journal-modal-subitems'>
+            <span style={{marginRight: 50}}>Track My Goal:</span>
+            <Checkbox
+              checked={!_.isEmpty(journalData) && journalData.track_my_goal}
+              onChange={(e) => {
+                setjournalData({
+                  ...journalData,
+                  track_my_goal: !journalData.track_my_goal,
+                });
+              }}
+              color='primary'
+              className={classes.checkboxRoot}
+            />
+          </div>
+        )}
+        {!_.isEmpty(journalData) &&
+          journalData.type === 'goal' &&
+          journalData.track_my_goal && (
+            <div>
+              <div className='journal-modal-subitems'>
+                <span className='dates'>
+                  <span style={{marginRight: 50, paddingTop: 10}}>
+                    Start Date:
+                  </span>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    format='MM/DD/yyyy'
+                    className={classes.datePicker}
+                    value={
+                      !_.isEmpty(journalData) && journalData.start_date
+                        ? journalData.start_date
+                        : moment().format('YYYY-MM-DD')
+                    }
+                    onChange={(e) => {
+                      console.log('on changing date', e);
+                      setjournalData({
+                        ...journalData,
+                        start_date: e,
+                      });
+                    }}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                </span>
+              </div>
+              <div className='journal-modal-subitems'>
+                <span className='dates'>
+                  <span style={{marginRight: 50, paddingTop: 10}}>
+                    End Date:
+                  </span>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    format='MM/DD/yyyy'
+                    className={classes.datePicker}
+                    value={
+                      !_.isEmpty(journalData) && journalData.end_date
+                        ? journalData.end_date
+                        : moment().format('YYYY-MM-DD')
+                    }
+                    onChange={(e) => {
+                      console.log('on changing date', e);
+                      setjournalData({
+                        ...journalData,
+                        end_date: e,
+                      });
+                    }}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                </span>
+              </div>
+            </div>
+          )}
         <div
           style={{
             display: 'flex',
-            justifyContent: 'space-around',
+            justifyContent: 'space-evenly',
             marginTop: 60,
           }}>
           <Button
             onClick={() => props.onClose()}
             variant='contained'
             color='secondary'
-            style={{width: 100}}>
+            style={{width: 80}}>
             Close
           </Button>
           <Button
             onClick={() => addJournal()}
             variant='contained'
             color='primary'
-            style={{width: 100}}>
+            style={{width: 80}}>
             Save
           </Button>
         </div>
