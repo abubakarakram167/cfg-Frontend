c0f2e41f17459c85ecd4e9293205eed0854d8c0b
abubakar <abubakarakram167@gmail.com>
2021-10-10 21:13:02 +0500

    Fin parser spell issue

--- src/components/sunEditor.js
+++ src/components/sunEditor.js
@@ -214,7 +214,7 @@ export default (props) => {
       $('._se_anchor_download')
         .parent()
         .after(
-          `<input class = "se-dialog-btn-check _se_anchor_url" id = "smart_link" style = "margin-left: 20px;" type="checkbox" ><span style = "font-weight: 600; font-size: 14px;" for = "smart_link" >smart link</span>`,
+          `<input class = "se-dialog-btn-check _se_anchor_url" id = "smart_link" style = "margin-left: 20px;" type="checkbox" ><span style = "font-weight: 600; font-size: 14px;" for = "smart_link" >Smart LINK</span>`,
         );
     }
 
