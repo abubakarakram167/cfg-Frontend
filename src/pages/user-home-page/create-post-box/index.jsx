81ebe19d38a9428ade5d682cf72ec1c296e35fb0
abubakar <abubakarakram167@gmail.com>
2021-11-12 01:42:52 +0500

    Fix video issue in timeline Posts.

--- src/pages/user-home-page/create-post-box/index.jsx
+++ src/pages/user-home-page/create-post-box/index.jsx
@@ -69,6 +69,8 @@ export default function CreatePostBox() {
   };
 
   const mediaJSX = () => {
+    console.log('the media type::', mediaType);
+    console.log('the media', media);
     switch (mediaType) {
       case 'image':
         return <img src={media} width='100%' />;
@@ -144,7 +146,12 @@ export default function CreatePostBox() {
               <button className='create-post-button' onClick={handleSave}>
                 <Save /> <span className='app-card-bottom-text'>Save</span>
               </button>
-              <div className='create-post-action-icons'>
+              <div
+                className='create-post-action-icons'
+                onClick={() => {
+                  setShowDialogue(true);
+                  setMediaType('video');
+                }}>
                 <Videocam style={{color: 'red'}} />
               </div>
               <div
@@ -154,10 +161,10 @@ export default function CreatePostBox() {
                 }}
                 className='create-post-action-icons'>
                 <PermMedia
-                  onClick={() => {
-                    setShowDialogue(true);
-                    setMediaType('video');
-                  }}
+                  // onClick={() => {
+                  //   setShowDialogue(true);
+                  //   setMediaType('video');
+                  // }}
                   style={{color: 'red'}}
                 />
               </div>
