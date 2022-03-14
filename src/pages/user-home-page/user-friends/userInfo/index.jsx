7c3126c799d9675fbf76ee272c040e5c1999b329
abubakar <abubakarakram167@gmail.com>
2022-03-15 03:01:17 +0500

    complete cfg family successfully

--- src/pages/user-home-page/user-friends/userInfo/index.jsx
+++ src/pages/user-home-page/user-friends/userInfo/index.jsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import {Card} from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
 import './style.css';
+import {getSignedUrl} from 'redux/actions/media';
+import NoUserProfile from 'assets/accountProfile.png';
+import Friend from 'redux/services/friends';
+import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
 
-const UserInfo = (props) => {
+const UserInfo = ({user, addFriend, toggleReloadData}) => {
+  const [avatarImage, setAvatarImage] = useState(null);
+  const [showMessage, setShowMessage] = useState(null);
+
+  useEffect(() => {
+    async function getUserDetails() {
+      getSignedUrl({
+        fileName: user && user.photo_url ? user.photo_url : '',
+      }).then((res) => {
+        setAvatarImage(res.newUrl);
+      });
+    }
+    getUserDetails();
+  }, []);
+
+  const sentFriendRequest = () => {
+    console.log('send frienddd requestt', user);
+    Friend.sendFriendRequest({userId: user.id})
+      .then((res) => {
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 2000);
+        toggleReloadData();
+      })
+      .catch((err) => {
+        console.log('the error', err);
+      });
+  };
+
+  const acceptFriendRequest = async () => {
+    try {
+      const data = await Friend.approveFriendRequest({userId: user.id});
+      console.log('after accept', data);
+      toggleReloadData();
+    } catch (err) {
+      console.log('the err in accepting friend', err);
+    }
+  };
+
+  console.log('the user', user);
+
   return (
     <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
       <div className='user-card-container'>
         <div className='user-image'>
           <img
-            style={{borderRadius: 20}}
-            src={
-              'https://du1jzqmqkepz6.cloudfront.net/2021-10-05T00-31-45.921Z-6128fA2NuZL.jpg?Expires=1647194506&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kdTFqenFtcWtlcHo2LmNsb3VkZnJvbnQubmV0LzIwMjEtMTAtMDVUMDAtMzEtNDUuOTIxWi02MTI4ZkEyTnVaTC5qcGciLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NDcxOTQ1MDZ9fX1dfQ__&Signature=TYRuYcdulQHb7oPAZpsGw3gmixxA50kHGKDXE9~9Gj5YuZpaJn~id6l3MHOYDftIjjOtHmSOnV7OtxADRdgL-9XGEC~H7B8NUr5fDGgCW0kaV0dZNORejMamfXQPKHWb6BUA5QFziqoLHrfM9fW-re1LDmq7ZCYRRTd501Jdd6~wn7wmOgfIxf1r1tNt5VXnn5QsQ7lTcircMrMPQdUiPNOVLEJPlcU640fWEGLqaWqxHPBaER8uai8NtQ0xFZQVjBmFSZ-xuq5Mdgb9thTrUF6kx1Gt7gi99Rbys5ZamT8IGcg1RDrS2r56dhRYdAthi~i-rdix7GGmQ-NyZvMEkw__&Key-Pair-Id=K30S0N0WWH7I01'
-            }
+            style={{borderRadius: 20, width: '100%', height: '100%'}}
+            src={user && user.photo_url ? avatarImage : NoUserProfile}
           />
         </div>
-        <h3 className='avatar-name'>Lenox james</h3>
+        <h3 className='avatar-name'>
+          {user ? user.first_name + user.last_name : 'not Available'}
+        </h3>
         <div className='button-container'>
-          <button className='confirm-buttton'>Confirm</button>
+          {addFriend ? (
+            <button onClick={sentFriendRequest} className='confirm-buttton'>
+              AddFriend
+            </button>
+          ) : (
+            <button onClick={acceptFriendRequest} className='confirm-buttton'>
+              Confirm
+            </button>
+          )}
         </div>
       </div>
+      {showMessage && (
+        <div>
+          <Snackbar
+            open={true}
+            autoHideDuration={2000}
+            onClose={() => setShowMessage(false)}>
+            <Alert variant='filled' severity='success'>
+              Friend Request Sent.
+            </Alert>
+          </Snackbar>
+        </div>
+      )}
     </div>
   );
 };
