ae03cc0fcac2f376959ffe217f7cb4b394aafc79
Hassan Yousaf <hassanyousaf565@gmail.com>
2022-05-24 02:25:12 +0500

    Seesion expired restored

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 const environment = process.env.NODE_ENV || 'development';
 var url = '';
 if (environment === 'development') {
-  url = 'https://devapp.mycfg.org/';
+  url = 'http://localhost:3690';
 } else url = 'https://devapp.mycfg.org/';
 
 export default url;
