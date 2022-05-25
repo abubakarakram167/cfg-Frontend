82483af9373774a5161f87267280a4154008603d
hassan493 <55235049+hassan493@users.noreply.github.com>
2022-05-25 05:48:54 +0500

    Update url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 const environment = process.env.NODE_ENV || 'development';
 var url = '';
 if (environment === 'development') {
-  url = 'https://devapp.mycfg.org/';
-} else url = 'https://devapp.mycfg.org/';
+  url = 'https://app.mycfg.org/';
+} else url = 'https://app.mycfg.org/';
 
 export default url;
