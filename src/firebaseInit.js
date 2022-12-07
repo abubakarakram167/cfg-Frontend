1e6ae85267552a22fd5988fb325fe3ef90e8d79a
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2022-12-07 13:46:08 +0100

    set up with node v14

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -13,12 +13,12 @@ import {getMessaging, getToken, onMessage} from 'firebase/messaging';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -41,7 +41,9 @@ export {
 };
 
 export const getNodeToken = (setTokenFound) => {
-  return getToken(messaging, {vapidKey: process.env.FIREBASE_VAPIDKEY})
+  return getToken(messaging, {
+    vapidKey: process.env.REACT_APP_FIREBASE_VAPIDKEY,
+  })
     .then((currentToken) => {
       if (currentToken) {
         console.log('current token for client: ', currentToken);
